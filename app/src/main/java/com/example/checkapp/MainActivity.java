3276b811e82ebb95a6264433656ec2ec20dbcc66
BEN <jason161616@gmail.com>
2019-03-18 18:07:57 +0900

    19.03.18 basic calendar

---
+++ app/src/main/java/com/example/checkapp/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.checkapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    CalendarView calendar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initComponent();
+    }
+    void initComponent(){
+        calendar = findViewById(R.id.calendarView);
+        calendar.setFirstDayOfWeek(2);
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth){
+                Toast.makeText(MainActivity.this, year + "/" + (month + 1) + "/" + dayOfMonth, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    public void shaveButtonClick(View view) {
+        Intent mIntent = new Intent(getApplicationContext(), ShaveActivity.class);
+        startActivity(mIntent);
+    }
+
+}
